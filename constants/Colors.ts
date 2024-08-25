557078725cbe999f1aae1d7e92fb2ffcde3af957
Brice Vignal <vignal.brice@gmail.com>
2024-08-25 18:29:17 +0200

    feat(Colors): add colors constant

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -3,13 +3,13 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
-const tintColorLight = '#0a7ea4';
+const tintColorLight = '#15BE77';
 const tintColorDark = '#fff';
 
 export const Colors = {
   light: {
-    text: '#11181C',
-    background: '#fff',
+    text: '#09051C',
+    background: "#F4F4F4",
     tint: tintColorLight,
     icon: '#687076',
     tabIconDefault: '#687076',
